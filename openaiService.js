8fd18f990810aa786c016cb0361ac59ce2a7efc2
DARKxKNIGHTx <m.karthikyadhav111@Gmail.com>
2025-02-19 11:20:45 +0530

    hi

--- openaiService.js
+++ openaiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "";
 
 export const getChatGPTResponse = async (userInput) => {
   try {
